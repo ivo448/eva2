f6cbd74eb5df9eb9240f3b4e8f7df7a92f2441ed
Ignacio <ignaciovaldiviaoyarce@hotmail.com>
2025-06-03 11:35:43 -0400

    commit final

--- script3.js
+++ script3.js
@@ -5,9 +5,47 @@ let datos = []
 
 function validar(){
     let eTelefono = document.getElementById("telefono")
-    let eText = document.getElementById("text")
     let ePwd = document.getElementById("password")
 
-    vTelefono.value = eTelefono
-    
+    let vTelefono = eTelefono.value
+    let vPwd = ePwd.value
+
+    if (vTelefono.length == 8 && vPwd.length > 5){
+        console.log("Telefono y contraseña valido")
+
+        let listadatos = {
+            telefono:vTelefono,
+            pwd:vPwd
+        }
+
+        let datosMap = listadatos.map((dato,index) => {
+            return "<tr><th>"+dato.telefono+"</th><th>"+datos.pwd+"</th><tr>"
+        })
+
+        datosStr = datos.join("")
+        return True
+    }else{
+        console.log("Telefono o contraseña no valido")
+        return False
+    }
+}
+
+function actualizar(indice){
+    let eTelefono = document.getElementById("telefono")
+    let ePwd = document.getElementById("password")
+
+    let datos = datos.filter((dato,index) => {
+        if (index == indice){
+            return "<tr><th>"
+        }
+    })
+}
+
+function eliminar(indice){
+
+    let datos = datos.filter((dato,index) => {
+        if (index == indice){
+            return True
+        }
+    })
 }
