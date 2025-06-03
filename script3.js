70992c703628464aa9540f596ffc91baa35f9f4f
Ignacio <ignaciovaldiviaoyarce@hotmail.com>
2025-06-03 10:43:37 -0400

    commit inicial

---
+++ script3.js
@@ -0,0 +1,13 @@
+//telefono debe tener exactamente 8 digitos, no es obligatoria
+//contrasenia debe tener mas de 5 caracteres, es obligatoria
+
+let datos = []
+
+function validar(){
+    let eTelefono = document.getElementById("telefono")
+    let eText = document.getElementById("text")
+    let ePwd = document.getElementById("password")
+
+    vTelefono.value = eTelefono
+    
+}
